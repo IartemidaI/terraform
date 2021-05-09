a32a3ae04845c120faa5f37b6056ff1155336862
Vitaliy Bernyk <65338532+IartemidaI@users.noreply.github.com>
2021-05-09 17:39:36 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable "aws_access_key" {
   type    = string
-  default = "aws_access_key"
+  default = ""
 }
 variable "aws_secret_key" {
   type    = string
-  default = "aws_secret_key"
+  default = ""
 }
